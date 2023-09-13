468534fd4fdc680816ac5b3e7fdade8165bc0ae5
Julio <julio@Julios-MBP.lan>
2023-09-13 19:27:42 +1000

    fix fig:min alias

--- frontend/project.clj
+++ frontend/project.clj
@@ -21,7 +21,7 @@
             "fig:min"   ["trampoline" "run" "-m" "figwheel.main"
                          "-co" "{:closure-defines {hnapp.model/SCRAPNEWS-URL \"api\"}
 :process-shim false}"
-                         "-O" "advanced" "-bo" "dev"]
+                         "-O" "advanced" "-bo" "min"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "hnapp.test-runner"]}
 
   :profiles {:dev
