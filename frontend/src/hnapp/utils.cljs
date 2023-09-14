7fdc81df8fc5076cb6ed20cd44006a14639dca50
Julio <julio@Julios-MBP.lan>
2023-09-15 08:31:56 +1000

    fix react/dom-node deprecation

--- frontend/src/hnapp/utils.cljs
+++ frontend/src/hnapp/utils.cljs
@@ -25,16 +25,22 @@
    (ocall (form-elem form elem) "item" n)))
 
 (defn with-init [elem mount-fn & {:keys [did-update] :as opts}]
-  (r/create-class
-   {:display-name "with-init"
-    :reagent-render
-    (fn [elem _ & {:as opts}] elem)
-    :component-did-update
-    (fn [this old-argv old-state snapshot]
-      (when (fn? did-update) (did-update this old-argv old-state snapshot)))
-    :component-did-mount
-    (fn [this]
-      (mount-fn (rd/dom-node this)))}))
+  (let [!el-ref (r/atom nil)
+        update-ref (fn [el] (reset! !el-ref el))]
+    (r/create-class
+     {:display-name "with-init"
+      :reagent-render
+      (fn [elem _ & {:as opts}]
+        (let [[el attrs & children] elem]
+          (if (map? attrs)
+            (vec (cons el (cons (assoc attrs :ref update-ref) children)))
+            (vec (cons el (cons {:ref update-ref} (cons attrs children)))))))
+      :component-did-update
+      (fn [this _old-argv _old-state _snapshot]
+        (when (fn? did-update) (did-update this {:ref @!el-ref})))
+      :component-did-mount
+      (fn [_]
+        (mount-fn @!el-ref))})))
 
 (defn tabs [content & {:keys [on-show active-id]}]
   (r/with-let [inst (r/atom nil)
@@ -42,33 +48,6 @@
                                             (some->> on-show (hash-map "onShow") clj->js)))
                update-fn #(some->> (r/argv %) last (spy "activating:") name (ocall @inst "select"))]
     [with-init content init-fn :did-update update-fn :active-id active-id]))
-
-(defn- init-modal [this show-ref modal-ref]
-  (if @modal-ref
-    (ocall @modal-ref "open")
-    (when-let [dom (rd/dom-node this)]
-      (-> modal-ref
-          (reset! (-> js/M.Modal
-                      (ocall "init" dom
-                             #js{"onCloseEnd" #(do (reset! show-ref nil) (reset! modal-ref nil))})))
-          (ocall "open")))))
-
-(defn modal [show-ref content]
-  (r/with-let [modal-obj (atom nil)]
-    (r/create-class
-     {:display-name "modal"
-      :reagent-render
-      (fn [show-ref content]
-        (when @show-ref content))
-      :component-did-update
-      (fn [this old-argv old-state snapshot]
-        (let [[_ show-ref content] (r/argv this)]
-          (if @show-ref
-            (init-modal this show-ref modal-obj)
-            (and @modal-obj (ocall @modal-obj "close")))))
-      :component-did-mount
-      (fn [this]
-        (init-modal this show-ref modal-obj))})))
 
 (defn with-value [f]
   (fn [e]
@@ -108,10 +87,9 @@
 (defn select-field [{:keys [doc path on-change] :as attrs} & options]
   (r/with-let [did-mount #(ocall js/M.FormSelect "init" %)
                did-update
-               (fn [this _ _ _]
-                 (let [el (rd/dom-node this)
-                       form-select (oget el "M_FormSelect")]
-                   (when-not (= (oget el "value") (first (ocall form-select "getSelectedValues")))
+               (fn [_ {:keys [ref]}]
+                 (let [form-select (oget ref "M_FormSelect")]
+                   (when-not (= (oget ref "value") (first (ocall form-select "getSelectedValues")))
                      (ocall form-select "_handleSelectChange"))))
                on-change (on-change-for doc path (:type attrs) on-change)]
     [with-init
@@ -158,7 +136,6 @@
                   (ocall (oget e "target") "setCustomValidity" "Please select a valid value")))]
     [with-init [input-field (assoc attrs :on-change on-change :auto-complete "off" :on-input on-input)]
      did-mount-fn]))
-
 
 (defn on-scroll-bottom [f & args]
   (fn [e]
